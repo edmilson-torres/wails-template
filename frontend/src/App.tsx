5951ebb64a8a31a947a582be2eddba979f22d18e
Edmilson Torres <edmilson.torres@gmail.com>
2025-08-03 18:22:30 -0300

    chore: formatter config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
 
 function App() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-24 gap-5">
-      <h1 className="text-3xl font-bold">Wails Template</h1>
-      <p>Vite, React, Typescript, Tailwind CSS, Shadcn, ESlint and Prettier</p>
-      <Button>Click me</Button>
-    </div>
-  )
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-24 gap-5">
+            <h1 className="text-3xl font-bold">Wails Template</h1>
+            <p>
+                Vite, React, Typescript, Tailwind CSS, Shadcn, ESlint and
+                Prettier
+            </p>
+            <Button>Click me</Button>
+        </div>
+    )
 }
 
 export default App
