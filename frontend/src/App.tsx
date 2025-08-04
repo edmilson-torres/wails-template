e69979ce5f96b6a3b286c3cc97ef962b4c9f00b2
Edmilson Torres <edmilson.torres@gmail.com>
2025-08-03 21:29:02 -0300

    refactor: default window size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 
 function App() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-24 gap-5">
+        <div className="flex flex-col items-center justify-center min-h-screen p-10 gap-5">
             <h1 className="text-3xl font-bold">Wails Template</h1>
             <p>
                 Vite, React, Typescript, Tailwind CSS, Shadcn, ESlint and
