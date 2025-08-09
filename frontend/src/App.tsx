7735e514a83ff6d6d735f22b6df110ea7aa4e33b
Edmilson Torres <edmilson.torres@gmail.com>
2025-08-09 10:02:08 -0300

    feat: dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import { Button } from '@/components/ui/button'
+import { ModeToggle } from './components/mode-toggle'
+import { ThemeProvider } from './components/theme-provider'
 
 function App() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-10 gap-5">
-            <h1 className="text-3xl font-bold">Wails Template</h1>
-            <p>
-                Vite, React, Typescript, Tailwind CSS, Shadcn, ESlint and
-                Prettier
-            </p>
-            <Button>Click me</Button>
-        </div>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <div className="flex flex-col items-center justify-center min-h-screen p-10 gap-5">
+                <h1 className="text-3xl font-bold">Wails Template</h1>
+                <p>
+                    Vite, React, Typescript, Tailwind CSS, Shadcn, ESlint and
+                    Prettier
+                </p>
+                <Button>Click me</Button>
+                <ModeToggle />
+            </div>
+        </ThemeProvider>
     )
 }
 
