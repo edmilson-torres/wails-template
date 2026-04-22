fdbc5ba380b3d6035c103c104cb70db553951134
Edmilson Torres <edmilson.torres@gmail.com>
2026-04-21 21:54:57 -0300

    feat: enhance Header component with theme display and improve layout

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,10 +1,15 @@
+import { useTheme } from '@/hooks/useTheme'
 import { ModeToggle } from './mode-toggle'
+import { Badge } from './ui/badge'
 
 export function Header() {
+    const { theme } = useTheme()
+
     return (
         <div className="sticky top-0 z-index-100 shadow-md w-full backdrop-blur-sm p-5">
-            <div className="flex justify-between items-center max-w-screen-md mx-auto">
-                <h1 className="text-3xl font-bold">Wails Template</h1>
+            <div className="flex items-center max-w-3xl mx-auto gap-4">
+                <h1 className="text-3xl font-bold grow">Wails Template</h1>
+                <Badge>{theme.charAt(0).toUpperCase() + theme.slice(1)}</Badge>
                 <ModeToggle />
             </div>
         </div>
